738c05eb7553cc1c02b65ce84cb3e18abc072c68
JamieGainer <jgainer137@gmail.com>
2018-12-19 09:56:35 -0800

    Started game class

---
+++ Game.py
@@ -0,0 +1,13 @@
+""" Placeholder for docstring """
+
+class Game():
+"""Placeholder for docstring """
+
+    def __init__(self, board_height = 10, board_width = 10, 
+                 ship_dict = default_ship_dict):
+        self.board_height = board_height
+        self.board_width = board_width
+        self.ship_dict = default_ship_dict
+
+    def print_board(self):
+        pass
